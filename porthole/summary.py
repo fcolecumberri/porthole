54da4098ef64762d86c385e5c79792a08ea3e4b5
dgt84 <dgt84@users.sourceforge.net>
2003-12-11 21:58:50 +0000

    Added license/copyright info

--- porthole/summary.py
+++ porthole/summary.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 
+'''
+    Porthole Summary Class
+    Loads package info into a textview and makes it pretty
+
+    Copyright (C) 2003 Fredrik Arnerup and Daniel G. Taylor
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+'''
+
 import gtk, pango
 import portagelib
 from utils import load_web_page
