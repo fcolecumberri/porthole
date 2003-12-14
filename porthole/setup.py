a645ab7a83ea864c0428b7980b8d6c7e2f2164f3
farnerup <farnerup@users.sourceforge.net>
2003-12-14 16:51:04 +0000

    Install porthole.py as a script and install COPYING in /usr/share/doc/porthole-0.1

--- porthole/setup.py
+++ porthole/setup.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 
-#import distutils
 from distutils.core import setup
 #pull the version directly from porthole
 from porthole import version
+
+datadir = "porthole/"
 
 setup(name = "Porthole",
       version = version,
       description = "GTK+ frontend to Portage",
       author = "Fredrik Arnerup and Daniel G. Taylor",
       # probably a good idea to use these emails
-      author_email = "farnerup@users.sourceforge.net"\
-                     ", dgt84@users.sourceforge.net",
+      author_email = ("farnerup@users.sourceforge.net"
+                      ", dgt84@users.sourceforge.net"),
       url = "http://porthole.sourceforge.net",
       # py_modules is a list of all our python modules
       py_modules = ["about", "depends", "metadata",
-                    "portagelib", "porthole", "process",
+                    "portagelib", "process",
                     "mainwindow", "summary", "utils"],
+      scripts = ["porthole.py"],
       # data_files is a list of non-python files we need
       # and where to install them
-      data_files = [("pixmaps",
-                    ["pixmaps/porthole-about.png",
-                    "pixmaps/porthole-icon.png"]),
-                    ("",
-                    ["porthole.glade"])]
+      data_files = [(datadir + "pixmaps",
+                     ["pixmaps/porthole-about.png",
+                      "pixmaps/porthole-icon.png"]),
+                    (datadir,
+                     ["porthole.glade"]),
+                    ("doc/porthole-" + version, ["COPYING"])
+                    ]
      )
