37aedc7fdab2ca41e5395de3ec661c3f17c7ced2
dol-sen <dol-sen@users.sourceforge.net>
2005-09-02 14:35:02 +0000

    added a callback parameter, more debug statements.

--- porthole/sterminal.py
+++ porthole/sterminal.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
-#
-
-#!/usr/bin/env python
 
 '''
-    Profuse plug-in interface to Porthole
+    sterminal
     Runs a command without a user interface
 
     Copyright (C) 2003 - 2005 Brian Dolbec, Fredrik Arnerup and Daniel G. Taylor
@@ -36,56 +33,59 @@ from process_reader import ProcessOutputReader
 
 
 class SimpleTerminal:
-	"""Porthole's simple terminal to run a command without an interface
-	"""
-	def __init__(self, command, need_output):
-		self.command = command
-		self.pid = None
-		# create the process reader
-		self.reader = ProcessOutputReader(Dispatcher(self.cleanup))
-		self.reader.record_output = need_output
-		# start the reader
-		self.reader.start()
+    """Porthole's simple terminal to run a command without an interface
+    """
+    def __init__(self, command, need_output, callback = None):
+        self.command = command
+        self.pid = None
+        # create the process reader
+        self.reader = ProcessOutputReader(Dispatcher(self.cleanup))
+        self.reader.record_output = need_output
+        self.callback = callback
+        # start the reader
+        self.reader.start()
 
-	def _run(self):
-		dprint("STERMINAL: run_app()")
-		env = utils.environment()
-		# next section probably not needed since this will usually be run one time only
-		if self.reader.fd:
-			if os.isatty(self.reader.fd):
-				dprint("STERMINAL: self.reader already has fd, closing")
-				os.close(self.reader.fd)
-			else:
-				dprint("STERMINAL: self.reader has fd but seems to be already closed.")
-				try:
-					os.close(self.reader.fd)
-				except OSError, e:
-					dprint("STERMINAL: error closing self.reader.fd: %s" % e)
-		
-		self.pid, self.reader.fd = pty.fork()
-		if self.pid == pty.CHILD:  # child
-			try:
-				# run the command
-				shell = "/bin/sh"
-				os.execve(shell, [shell, '-c', self.command],
-					env)
-			except Exception, e:
-				dprint("STERMINAL: Error in child" + e)
-				os._exit(1)
+    def _run(self):
+	dprint("STERMINAL: run_app()")
+	env = utils.environment()
+	# next section probably not needed since this will usually be run one time only
+	if self.reader.fd:
+		if os.isatty(self.reader.fd):
+			dprint("STERMINAL: self.reader already has fd, closing")
+			os.close(self.reader.fd)
 		else:
-			# set process_running so the reader thread reads it's output
-			self.reader.process_running = True
-			dprint("STERMINAL: pty process id: %s ******" % self.pid)
-		return
-
-	def cleanup(self):
-		# reset to None, so next one starts properly
-		self.reader.fd = None
-		self.reader.die = True
-		# clean up finished process
+			dprint("STERMINAL: self.reader has fd but seems to be already closed.")
+			try:
+				os.close(self.reader.fd)
+			except OSError, e:
+				dprint("STERMINAL: error closing self.reader.fd: %s" % e)
+	
+	self.pid, self.reader.fd = pty.fork()
+	if self.pid == pty.CHILD:  # child
 		try:
-			m = os.waitpid(self.pid, 0) # wait for any child processes to finish
-			dprint("STERMINAL: process %s finished, status %s" % m)
-		except OSError, e:
-			if not e.args[0] == 10: # 10 = no process to kill
-				dprint("STERMINAL: OSError %s" % e)
+			# run the command
+			shell = "/bin/sh"
+			os.execve(shell, [shell, '-c', self.command],
+				env)
+		except Exception, e:
+			dprint("STERMINAL: Error in child" + e)
+			os._exit(1)
+	else:
+		# set process_running so the reader thread reads it's output
+		self.reader.process_running = True
+		dprint("STERMINAL: pty process id: %s ******" % self.pid)
+	return
+
+    def cleanup(self):
+        # reset to None, so next one starts properly
+        self.reader.fd = None
+        self.reader.die = True
+        # clean up finished process
+        try:
+            m = os.waitpid(self.pid, 0) # wait for any child processes to finish
+            dprint("STERMINAL: process %s finished, status %s" % m)
+        except OSError, e:
+            if not e.args[0] == 10: # 10 = no process to kill
+                dprint("STERMINAL: OSError %s" % e)
+        if self.callback:
+            self.callback()
