6804aa7318a1aeaff990a05737ea9683a928768b
mesilliac <mesilliac@users.sourceforge.net>
2005-09-11 07:16:28 +0000

    added list of modules (for testing: "from porthole import *" now makes sense)

---
+++ porthole/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['mainwindow', 'about', 'advemerge', 'command', 'config', 'depends',
+    'dispatcher', 'loaders', 'metadata', 'plugin', 'portagelib',
+    'process_reader', 'readers', 'set_config', 'sterminal', 'summary',
+    'terminal', 'utils', 'version', 'version_sort']
