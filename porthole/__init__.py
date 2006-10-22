b4c8ad37fef2c02fa4ab09e51e2e1b6b1ffd4c46
dol-sen <dol-sen@users.sourceforge.net>
2006-10-22 06:06:50 +0000

    added new packagebook directory.

--- porthole/__init__.py
+++ porthole/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ['mainwindow', 'about', 'advemerge', 'command', 'config', 'depends',
-    'dispatcher', 'loaders', 'metadata', 'plugin', 'portagelib',
+    'dispatcher', 'loaders', 'metadata', 'packagebook', 'plugin', 'portagelib',
     'process_reader', 'readers', 'set_config', 'sterminal', 'summary',
     'terminal', 'utils', 'version', 'version_sort', 'views', 'xmlmgr']
