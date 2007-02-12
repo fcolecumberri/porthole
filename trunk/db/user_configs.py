e556e56051121561de765500a9cc2ca7489a26c6
dol-sen <dol-sen@users.sourceforge.net>
2007-02-12 06:59:36 +0000

    moved user_configs.py from backends to db. updated to work with /etc/portage/package.* directories.

---
+++ trunk/db/user_configs.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python
+
+'''
+    Porthole user_configs module
+    Holds all portage user config data functions for Porthole
+
+    Copyright (C) 2006  Brian Dolbec
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+'''
+
+import os
+
+import config
+from backends.utilities import read_bash, reduce_flags
+from utils.dispatcher import Dispatcher
+from sterminal import SimpleTerminal
+from dialogs.fileselector import FileSelector
+import backends
+portage_lib = backends.portage_lib
+
+import utils.debug
+
+## set up this module to act as a user configs data server,
+## watch the user configs for changes and auto update
+
+CONFIG_TYPES = ['USE', 'KEYWORDS', 'MASK', 'UNMASK', 'SETS']
+CONFIG_FILES = ['package.use', 'package.keywords', 'package.mask', 'package.unmask', 'sets']
+
+def get_type(file):
+    if file:
+        parts = file.split('/')
+        if parts[-1] not in CONFIG_FILES:
+            parts = parts[:-1]
+        if parts[-1] not in CONFIG_FILES:
+            utils.debug.dprint("USER_CONFIGS: get_type(); failed to determine config type for: " + file)
+        else:
+            return CONFIG_TYPES[CONFIG_FILES.index(parts[-1])]
+
+class ConfigAtom:
+    def __init__(self, name):
+        self.name = name                        # pkg name
+        self.type = None                         # package.use, package.keywords, etc.
+        self.version = ''                          # specific version if defined
+        self.atoms = ''                            # any atoms if defined
+        self.value = []                              # any keywords, use flags, etc.
+        self.file = ''                                 # the file the atom was found in
+
+    def __repr__(self): # called by the "print" function
+        """Returns a human-readable string representation of the ConfigAtom
+        (used by the "print" statement)."""
+        line = self.acpv()
+        line = line + ' ' +' '.join(self.value)
+        line = line + '  ==> ' + self.file
+        return line
+
+    def get_line(self):
+        return self.__repr__()
+
+    def acpv(self):
+        _acpv = self.atoms +self.name
+        if self.version != '':
+            _acpv = _acpv + '-'+ self.version
+        return _acpv
+
+class UserConfigs:
+    """get and store all user configs data"""
+    
+    def __init__(self, go):
+        utils.debug.dprint("USER_CONFIGS: __init__()")
+        self.db = {}
+        for type in CONFIG_TYPES:
+            self.db[type] = {}
+        for file in CONFIG_FILES:
+            utils.debug.dprint("USER_CONFIGS: __init__(); file = " + file)
+            self.load(os.path.join(portage_lib.user_config_dir,file))
+
+    def load(self, myfilename, recursive = True):
+        lines = []
+        if recursive and os.path.isdir(myfilename):
+            dirlist = os.listdir(myfilename)
+            dirlist.sort()
+            for f in dirlist:
+                if not f.startswith("."):
+                    self.load(os.path.join(myfilename, f), recursive)
+        else:
+            lines = read_bash(myfilename)
+            self.atomize(lines, myfilename)
+
+    def atomize(self, lines, source):
+        """takes a list of items and creates db records of the package and values"""
+        type = get_type(source)
+        utils.debug.dprint("USER_CONFIGS: atomize(); type = " + type)
+        for line in lines:
+            values = line.split()
+            name,atoms, version = portage_lib.split_atom_pkg( values[0] )
+            atom = ConfigAtom(name)
+            atom.atoms = atoms
+            atom.version = version
+            atom.file = source
+            atom.type = type
+            atom.value = values[1:]
+            utils.debug.dprint("USER_CONFIGS: atomize(); new atom: " + str(atom.name))
+            if name in self.db[type]:
+                self.db[type][name].append(atom)
+            else:
+                self.db[type][name] = [atom]
+
+    def get_atom(self, type, name = None, ebuild = None):
+        """searches for a package name and or ebuild version
+            and returns the atom or None if not found"""
+        if type not in CONFIG_TYPES:
+            return None
+        result = None
+        if name and name != '':
+            if name in self.db[type]:
+                result = self.db[type][name]
+            else:
+                result = []
+        elif ebuild and ebuild != '':
+            utils.debug.dprint("USER_CONFIGS: get_atom(); ebuild = " + ebuild)
+            pkgname = portage_lib.get_full_name(ebuild)
+            utils.debug.dprint("USER_CONFIGS: get_atom(); pkgname = " + pkgname)
+            if pkgname in self.db[type]:
+                result = self.db[type][pkgname]
+            else:
+                result = []
+            # match ebuild version
+        utils.debug.dprint("USER_CONFIGS: get_atom(); result = " + str(result))
+        return result
+
+    def get_types(self):
+        """returns a list of the valid config types"""
+        return CONFIG_TYPES
+
+    def get_useflags(self, ebuild):
+        """returns a list of the useflags that are configured for the ebuild"""
+        atoms = self.get_atom('USE', None, ebuild)
+        myflags = []
+        for atom in atoms:
+            myflags = myflags + atom.value
+        myflags = reduce_flags(myflags)
+        return myflags
+
+    def get_unmask(self, ebuild):
+        pass
+        
+    def get_keywords(self, ebuild):
+        """returns a unique list of matching keywords configured for the ebuild"""
+        atoms = self.get_atom('KEYWORDS', None, ebuild)
+        keywords = []
+        for atom in atoms:
+            if atom.atoms != '':
+                # check for
+                pass
+            for x in atom.value:
+                if x not in keywords:
+                    keywords.append(x)
+        return keywords
+
+    def get_user_config(self, type, name=None, ebuild=None):
+        """
+        Function for parsing package.use, package.mask, package.unmask
+        and package.keywords.
+        
+        Returns /etc/portage/<file> as a dictionary of ebuilds, with
+        dict[ebuild] = list of flags.
+        If name is given, it will be parsed for ebuilds with xmatch('match-all'),
+        and only those ebuilds will be returned in dict.
+        
+        If <ebuild> is given, it will be matched against each line in <file>.
+        For package.use/keywords, a list of applicable flags is returned.
+        For package.mask/unmask, a list containing the matching lines is returned.
+        """
+        utils.debug.dprint("USER_CONFIGS: get_user_config('" + type + "')")
+        masktypes = ['MASK', 'UNMASK']
+        othertypes = ['USE', 'KEYWORDS']
+        package_types = othertypes + masktypes
+        if type not in package_types:
+            utils.debug.dprint("USER_CONFIGS: get_user_config(): unsupported config type '%s'" % type)
+            return None
+        atoms = self.get_atom(type, name, ebuild)
+        dict = {}
+        if ebuild is not None:
+            result = []
+            for atom in atoms:
+                acpv = atom.acpv()
+                match = portage_lib.xmatch('match-list', acpv, mylist=[ebuild])
+                if match:
+                    if type in masktypes:
+                        result.extend(acpv) # package.mask/unmask
+                    else:
+                        result.extend(atom.value[:]) # package.use/keywords
+            return result
+        if name:
+            target = portage_lib.xmatch('match-all', name)
+            for atom in atoms:
+                acpv = atom.acpv()
+                ebuilds = portage_lib.xmatch('match-all', acpv)
+                for ebuild in ebuilds:
+                    if ebuild in target:
+                        if ebuild in dict:
+                            dict[ebuild].extend(atom.value[:])
+                        else:
+                            dict[ebuild] = atom.value[:]
+        else:
+            for atom in atoms:
+                acpv = atom.acpv()
+                ebuilds = portage_lib.xmatch('match-all', acpv)
+                for ebuild in ebuilds:
+                    if ebuild in dict:
+                        dict[ebuild].extend(atom.value[:])
+                    else:
+                        dict[ebuild] = atom.value[:]
+        return dict
+
+    def set_user_config( self, type, name='', ebuild='', add='', remove='', callback=None, parent_window = None):
+        """
+        Adds <name> or '=' + <ebuild> to <file> with flags <add>.
+        If an existing entry is found, items in <remove> are removed and <add> is added.
+        
+        If <name> and <ebuild> are not given then lines starting with something in
+        remove are removed, and items in <add> are added as new lines.
+        """
+        utils.debug.dprint("USER_CONFIGS: set_user_config()")
+        command = ''
+        if type not in CONFIG_TYPES:
+            utils.debug.dprint("USER_CONFIGS: set_user_config(): unsupported config type '%s'" % type)
+            return False
+        config_path = portage_lib.user_config_dir
+        # get an existing atom if one exists.  pass both name and ebuild, no need to check which one, I think
+        atom = self.get_atom(type, name, ebuild)
+        if atom == None or atom == []: # get a target file
+            target = CONFIG_FILES[CONFIG_TYPES.index(type)]
+            target_path = os.path.join(portage_lib.user_config_dir,file)
+            if os.path.isdir(file): # Then bring up a file selector dialog
+                if parent_window == None:
+                    parent_window = config.Mainwindow
+                file_picker = FileSelector(parent_window, target_path)
+                file = file_picker.get_filename(_("Porthole: Please select the %s file to use for adding/changing %s") \
+                                                                %(target, atom.name))
+            utils.debug.dprint("USER_CONFIGS: set_user_config(): got a filename :) file = " + file)
+
+        else: # found one
+            file = atom[0].file
+            utils.debug.dprint("USER_CONFIGS: set_user_config(): found an atom :) file = " + file)
+
+        if isinstance(add, list):
+            add = ' '.join(add)
+        if isinstance(remove, list):
+            remove = ' '.join(remove)
+        if not os.access(config_path, os.W_OK):
+            commandlist = [config.Prefs.globals.su, '"python', config.Prefs.DATA_PATH + 'backends/set_config.py -d -f ' + file]
+            if name != '':
+                commandlist.append('-n %s' %name)
+            if ebuild != '':
+                commandlist.append('-e %s' %ebuild)
+            if add != '':
+                items = add.split()
+                for item in items:
+                    commandlist.append('-a %s' % item)
+            if remove != '':
+                items = remove.split()
+                for item in items:
+                    commandlist.append('-r %s' % item)
+            command = ' '.join(commandlist) + '"'
+            utils.debug.dprint(" * USER_CONFIGS: set_user_config(): command = %s" %command )
+            # add code to update_config()
+            if not callback: callback = reload_portage
+            app = SimpleTerminal(command, False, dprint_output='SET_USER_CONFIG CHILD APP: ', callback=Dispatcher(callback))
+            app._run()
+        else:
+            add = add.split()
+            remove = remove.split()
+            set_config.set_user_config(file, name, ebuild, add, remove)
+            if callback: callback()
+            else: reload_portage()
+        # This is slow, but otherwise portage doesn't notice the change.
+        #reload_portage()
+        # Note: could perhaps just update portage.settings.
+        # portage.settings.pmaskdict, punmaskdict, pkeywordsdict, pusedict
+        # or portage.portdb.mysettings ?
+        return True
+
