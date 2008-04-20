595ab5706bc2cdecb9b80725f8cab702c95f1f43
dol-sen <dol-sen@users.sourceforge.net>
2008-04-20 06:38:22 +0000

    more moves to the end goal

--- trunk/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ['mainwindow', 'advancedemerge', 'backends', 'config', 'dialogs',
-    'loaders', 'package', 'packagebook', 'readers',  'startup', 'sterminal',
-    'terminal', 'utils', 'version', 'views', '_xml']
