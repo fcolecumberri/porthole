d0457b31cfc7ae2573c7c782cfdbd0ee2fcf1f33
dol-sen <dol-sen@users.sourceforge.net>
2008-04-25 17:31:50 +0000

    fix copyright in the gladefile, add gettext support to the version.copyright variable

--- porthole/version.py
+++ porthole/version.py
@@ -18,7 +18,8 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
+from gettext import gettext as _
 
 version = "0.6.0_rc2"
 
-copyright = "Copyright (c) 2003 - 2008"
+copyright = _("Copyright (c) 2003 - 2008")
