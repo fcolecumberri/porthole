b3f064b76ff4de54562a0306352f52781518c337
dol-sen <dol-sen@users.sourceforge.net>
2008-04-26 02:36:47 +0000

    fix up the commands better

--- scripts/dopot.sh
+++ scripts/dopot.sh
@@ -4,15 +4,15 @@
 #
 # Copyright 2005 - 2008  Tommy Iorns, Brian Dolbec
 #
-
-intltool-extract --type="gettext/glade" ../porthole/glade/porthole.glade
-intltool-extract --type="gettext/glade" ../porthole/glade/advemerge.glade
-intltool-extract --type="gettext/glade" ../porthole/glade/config.glade
-intltool-extract --type="gettext/glade" ../porthole/glade/about.glade
+cd ..
+intltool-extract --type="gettext/glade" porthole/glade/porthole.glade
+intltool-extract --type="gettext/glade" porthole/glade/advemerge.glade
+intltool-extract --type="gettext/glade" porthole/glade/config.glade
+intltool-extract --type="gettext/glade" porthole/glade/about.glade
 
 # translatable strings in .glade files extracted to .h files
-xgettext -k_ -kN_ -o ../porthole/i18n/messages.pot ../porthole/*.py  ../porthole/advancedemerge/*.py ../porthole/_xml/*.py ../porthole/backends/*.py ../porthole/config/*.py \
-    ../porthole/db/*.py ../porthole/dialogs/*.py ../porthole/loaders/*.py ../porthole/packagebook/*.py ../porthole/readers/*.py ../porthole/terminal/*.py  \
-    ../porthole/utils/*.py ../porthole/views/*.py  ../porthole/glade/*.h
+xgettext -k_ -kN_ -o porthole/i18n/messages.pot porthole/glade/*.h
+xgettext -k_ -kN_ -j -o porthole/i18n/messages.pot $(find -name "*.py")
+
 
 # all python files and .h files placed in i18n/messages.pot
