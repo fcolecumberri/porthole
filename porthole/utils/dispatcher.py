a5423be91ab6dffce33df471fbadbc2bd9e5f89b
dol-sen <dol-sen@users.sourceforge.net>
2009-04-11 00:59:04 +0000

    add a wait for reply dispatcher for interthread communication.

--- porthole/utils/dispatcher.py
+++ porthole/utils/dispatcher.py
@@ -25,6 +25,8 @@
 
 import gobject, os, Queue
 from select import select
+from sys import stderr
+import thread, threading
 
 class Dispatcher:
     """Send signals from a thread to another thread through a pipe
@@ -37,7 +39,7 @@ class Dispatcher:
         self.queue = Queue.Queue(0) # thread safe queue
         self.pipe_r, self.pipe_w = os.pipe()
         gobject.io_add_watch(self.pipe_r, gobject.IO_IN, self.on_data)
-        
+
     def __call__(self, *args):
         """Emit signal from thread"""
         self.queue.put(args)
@@ -53,3 +55,49 @@ class Dispatcher:
                 self.callback(*self.queue.get(), **self.callback_kwargs)
         return self.continue_io_watch
 
+class Dispatch_wait:
+    """Send signals from a thread to another thread through a pipe
+    in a thread-safe manner. wait for data to return"""
+    def __init__(self, callback_func, *args, **kwargs):
+        self.callback = callback_func
+        self.callback_args = args
+        self.callback_kwargs = kwargs
+        self.continue_io_watch = True
+        self.callqueue = Queue.Queue(0) # thread safe queue
+        self.reply = Queue.Queue(0)
+        self.callpipe_r, self.callpipe_w = os.pipe()
+        self.wait = {}  # dict of boolleans for incoming thread id's waiting for replies
+        self.Semaphore = threading.Semaphore()
+        gobject.io_add_watch(self.callpipe_r, gobject.IO_IN, self.on_calldata)
+
+    def __call__(self, *args):  # this function is running in the calling thread
+        """Emit signal from thread"""
+        id = thread.get_ident()
+        self.queue.put([args, id])
+        # write to pipe afterwards
+        os.write(self.callpipe_w, "X")
+        # now wait for the reply
+        self.semaphore.aquire()
+        self.wait[id] = True
+        self.Semaphore.release()
+        while self.wait[myid]:
+            #pass the time waiting for a reply by having a snooze
+            time.sleep(0.01)
+        myreply. reply_id = self.reply.get()
+        if reply_id != id:
+            print >>stderr, "DISPATCH_WAIT:  Uh-Oh! id's do not match!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        return myreply
+        
+    
+    def on_calldata(self, source, cb_condition):
+        if select([self.callpipe_r],[],[], 0)[0] and os.read(self.callpipe_r,1):
+            args, id =self.queue.get()
+            if self.callback_args:
+                reply = self.callback(*( self.callback_args + args), **self.callback_kwargs)
+            else:
+                reply = self.callback(*args, **self.callback_kwargs)
+            self.semaphore.aquire()
+            self.reply.put([reply, id])
+            self.wait[id] = False
+            self.semaphore.release()
+        return self.continue_io_watch
