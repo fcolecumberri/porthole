afe8e7c7a8002e3fd1f7713ac52f9de32f0ca663
dol-sen <dol-sen@users.sourceforge.net>
2009-12-12 18:38:36 +0000

    fix unicode error in necoro's name, disable the connection to the "map" signal, comment out a debug stmt.

--- porthole/views/lazyview.py
+++ porthole/views/lazyview.py
@@ -3,12 +3,12 @@
 """ File: porthole/views/lazyview.py
 This file is part of the Porthole, a graphical portage-frontend.
 
-Copyright (C) 2006-2009 René 'Necoro' Neumann
+Copyright (C) 2006-2009 RenÃ© 'Necoro' Neumann
 This is free software.  You may redistribute copies of it under the terms of
 the GNU General Public License version 2.
 There is NO WARRANTY, to the extent permitted by law.
 
-Written by René 'Necoro' Neumann <necoro@necoro.net>
+Written by  RenÃ© 'Necoro' Neumann <necoro@necoro.net>
 Adapted to Porthole by, Brian Dolbec <dol-sen@users.sourceforge.net>
 """
 #from __future__ import absolute_import, with_statement
@@ -19,12 +19,13 @@ from porthole.utils import debug
 
 class LazyView (object):
     def __init__ (self):
-        self.connect("map", self.cb_mapped)
+        #self.connect("map", self.cb_mapped)
 
         self.pkg = None
         self.updated = False
 
     def update (self, pkg, force = False):
+        #debug.dprint("LazyView:, update(), pkg = " + pkg)
         self.pkg = pkg
         self.ebuild = None
         self.updated = True
