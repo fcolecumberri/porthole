887698f429f9fccb6538f971a23277da2017764c
dol-sen <dol-sen@users.sourceforge.net>
2010-02-21 03:41:36 +0000

    add turkish translation to setup.py add an __all__ to views/__init__ since it wasn't getting all the new files.

--- porthole/views/__init__.py
+++ porthole/views/__init__.py
@@ -21,3 +21,6 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 '''
+__all__ = ['category', 'changelog', 'commontreeview', 'depends', 'helpers', 'highlight',
+    'lazyview', 'list', 'markup', 'menus', 'models', 'package', 'sorts']
+    
